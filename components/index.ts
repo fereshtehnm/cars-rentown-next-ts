8a1646b354f2a0699500652a13c7f010317365bc
Fereshteh <fereshte.namazi.edu@gmail.com>
2024-04-14 21:52:07 +0330

    Fix footer

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,5 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
-import { Footer } from "./Footer";
+import Footer from "./Footer";
 import Button from "./CustomButton";
 export { Hero, Navbar, Footer, Button };
