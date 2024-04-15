019b00dfeeada61e65c3f4446b1404a1ae16f9c2
Fereshteh <fereshte.namazi.edu@gmail.com>
2024-04-15 22:38:31 +0330

    Fixes in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+
+  // Add basePath
+  basePath: "/github-pages",
+};
 
 export default nextConfig;
